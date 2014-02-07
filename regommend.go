0ab16cd70332da4d03f2a3bed764c1582a0e19c1
Christian Muehlhaeuser <muesli@gmail.com>
2014-02-07 18:31:00 +0100

    * Engine supports multiple data tables.

---
+++ regommend.go
@@ -0,0 +1,38 @@
+/*
+ * Simple recommendation engine
+ *     Copyright (c) 2014, Christian Muehlhaeuser <muesli@gmail.com>
+ *                   
+ *   For license see LICENSE.txt
+ */
+
+package regommend
+
+import (
+	"sync"
+)
+
+var (
+	tables = make(map[string]*RegommendTable)
+	mutex sync.RWMutex
+)
+
+// Returns the existing engine table with given name or creates a new one
+// if the table does not exist yet.
+func Table(table string) *RegommendTable {
+	mutex.RLock()
+	t, ok := tables[table]
+	mutex.RUnlock()
+
+	if !ok {
+		t = &RegommendTable{
+			name:  table,
+			items: make(map[interface{}]*RegommendItem),
+		}
+
+		mutex.Lock()
+		tables[table] = t
+		mutex.Unlock()
+	}
+
+	return t
+}
