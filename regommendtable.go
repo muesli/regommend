0b0158cd6984ae7992a8caddc47c066a95973a29
Christian Muehlhaeuser <muesli@gmail.com>
2014-02-10 21:07:43 +0100

    * Basic engine skeleton.

--- regommendtable.go
+++ regommendtable.go
@@ -6,3 +6,34 @@
  */
 
 package regommend
+
+import (
+	"errors"
+	"log"
+	"fmt"
+	_ "sort"
+	"math"
+	"sort"
+	"sync"
+	_ "time"
+)
+
+// Structure of a table with items in the engine.
+type RegommendTable struct {
+	sync.RWMutex
+
+	// The table's name.
+	name string
+	// All items in the table.
+	items map[interface{}]*RegommendItem
+
+	// The logger used for this table.
+	logger *log.Logger
+
+	// Callback method triggered when trying to load a non-existing key.
+	loadData func(key interface{}) *RegommendItem
+	// Callback method triggered when adding a new item to the engine.
+	addedItem func(item *RegommendItem)
+	// Callback method triggered before deleting an item from the engine.
+	aboutToDeleteItem func(item *RegommendItem)
+}
