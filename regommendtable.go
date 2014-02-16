a13cf80104dbcc7e93651393a8e0d183cea71fda
Christian Muehlhaeuser <muesli@gmail.com>
2014-02-16 23:01:13 +0100

    * Cosine similarity comparison implementation.

--- regommendtable.go
+++ regommendtable.go
@@ -172,6 +172,30 @@ func (table *RegommendTable) Flush() {
 	table.items = make(map[interface{}]*RegommendItem)
 }
 
+func cosineSim(t1, t2 map[interface{}]float64) float64 {
+	sum_xy := 0.0
+	sum_x2 := 0.0
+	sum_y2 := 0.0
+
+	for key, x := range t1 {
+		y, ok := t2[key]
+		if ok {
+			fmt.Println("Found shared:", key, x, y)
+
+			sum_xy += x * y
+			sum_x2 += math.Pow(x, 2)
+			sum_y2 += math.Pow(y, 2)
+		}
+	}
+
+	denominator := math.Sqrt(sum_x2) * math.Sqrt(sum_y2)
+	if denominator == 0 {
+		return 0
+	}
+
+	return sum_xy / denominator
+}
+
 // Internal logging method for convenience.
 func (table *RegommendTable) log(v ...interface{}) {
 	if table.logger == nil {
